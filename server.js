67d630ae187ab0623f0da5f3c685cea475bdcdb7
smartnikhil24 <smartnikhil24@gmail.com>
2022-07-27 21:28:41 +0530

    added code in server.js

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ var PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('../src/pages/index.tsx');
-});
-
+//const app = express();
+//app.get('/', (req, res) => {
+  //res.send('../src/pages/index.tsx');
+//});
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Nikhilesh Resume',
+  tagline: 'This site has my Resume',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+}
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
